e4067a8c5a4a70feb254fa2f79df452383b78244
Robert West <rwest82396@gmail.com>
2025-02-15 14:18:11 +0100

    Complete 1A/1B for Gender

--- python/sql/gender_ranking_absolute_1a.sql
+++ python/sql/gender_ranking_absolute_1a.sql
@@ -16,13 +16,32 @@ WITH base AS (
 
 crs_totals AS (
     SELECT 
-        b.donor_name,
+        b.donor_name AS donor,
         b.year,
-        sum(coalesce(usd_disbursement_defl, 0) * dfl.deflator) AS bilateral_oda
+        sum(coalesce(usd_disbursement_defl, 0) / 100 * dfl.deflator) AS total_oda
     FROM base b 
     INNER JOIN "{{deflator_file}}" dfl ON dfl.donor = b.donor_name AND dfl.year = {{latest_year}}
     GROUP BY 1,2
+),
+
+ranked AS (
+    SELECT 
+        donor,
+        year,
+        sum(total_oda) AS total_oda,
+        row_number() OVER (ORDER BY sum(total_oda) DESC) AS rn
+    FROM crs_totals
+    GROUP BY 1,2
 )
 
-SELECT * 
-FROM crs_totals
+SELECT 
+    donor "Donor", 
+    Year, 
+    total_oda "ODA towards Gender",
+    CASE 
+        WHEN rn::TEXT LIKE '%1' AND rn != 11 THEN rn || 'st'
+        WHEN rn::TEXT LIKE '%2' AND rn != 12 THEN rn || 'nd'
+        WHEN rn::TEXT LIKE '%3' AND rn != 13 THEN rn || 'rd'
+        ELSE rn || 'th'
+    END AS "Ranking"
+FROM ranked
