5bd088b6885847143f1568ba2188a28146bbf21e
Robert West <rwest82396@gmail.com>
2025-02-26 19:09:42 +0100

    Finalize climate scripts

---
+++ python/sql/climate_intervention_type_split_3a.sql
@@ -0,0 +1,51 @@
+WITH base AS (
+    SELECT
+        donor_name,
+        year,
+        purpose_name,
+        purpose_code,
+        greatest(climate_mitigation, climate_adaptation) climate_total, 
+        usd_commitment_defl
+    FROM "{{crs_file}}"
+    WHERE year BETWEEN ({{latest_year}} - 4) AND ({{latest_year}})
+    AND donor_name IN {{dac_countries}}
+    AND flow_name IN (
+        'ODA Loans','Equity Investment','ODA Grants'
+    )
+    AND donor_name != 'EU Institutions'
+), 
+
+transformed AS (
+    SELECT 
+        b.donor_name,
+        b.year,
+        dsf.sector_renamed,
+        b.usd_commitment_defl / 100 * dfl.deflator AS bilateral_oda
+    FROM base b 
+    LEFT JOIN "{{dt_sector_file}}" dsf ON dsf.sector_code = b.purpose_code
+    LEFT JOIN "{{deflator_file}}" dfl ON dfl.donor = b.donor_name AND dfl.year = {{latest_year}}
+    WHERE b.climate_total IN (1,2)
+),
+
+allocable_totals AS (
+    SELECT 
+        "Donor_1" AS donor_name,
+        "TIME_PERIOD" AS year,
+        sum("OBS_VALUE") AS total_oda
+    FROM "{{riomarkers_file}}"
+    WHERE 1=1
+    AND year = ({{latest_year}})
+    --TODO: Change fund flow here for total
+    AND "Donor_1" IN {{dac_countries}}
+    AND "Donor_1" != 'EU Institutions'
+    GROUP BY 1,2
+),
+-- SELECT 
+--     donor_name AS donor,
+--     year,
+--     purpose_name AS Sector,
+--     sum(bilateral_oda) AS "Bilateral ODA for",
+--     sum(bilateral_oda) * 100 / sum(sum(bilateral_oda)) OVER (PARTITION BY donor_name, year) AS share
+-- FROM transformed
+-- GROUP BY 1,2,3
+-- ORDER BY 2 DESC, 4 DESC
