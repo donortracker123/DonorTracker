395ccf7126fa410208932aaf8d0791194e50ca25
Robert West <rwest82396@gmail.com>
2025-03-04 23:08:42 +0100

    Deflate bimulti

--- python/sql/bimulti_4.sql
+++ python/sql/bimulti_4.sql
@@ -1,63 +1,76 @@
 WITH base AS (
-        SELECT 
-            "Donor_1" AS donor,
-            "Year" AS year,
-            "Aid type" AS aid_type,
-            "VALUE" AS value
-        FROM "{{dac1_file}}"
-        WHERE 1=1
-        AND year BETWEEN ({{latest_year}} - 4) AND ({{latest_year}})
-        AND "Amount type" = 'Constant Prices (2022 USD millions)'
-        AND "Fund flows" = 'Grant equivalents'
-        AND "Aid type" IN (
-            'I. Official Development Assistance (ODA) (I.A + I.B)',
-            'I.B. Multilateral Official Development Assistance (capital subscriptions are included with grants)',
-            'I.A. Memo: ODA channelled through multilateral organisations',
-            'I.A. Bilateral Official Development Assistance by types of aid (1+2+3+4+5+6+7+8+9+10)'
-        )
-    ), 
-
-    filtered AS (  
-        SELECT
-            donor,
-            year AS "Year",
-            SUM(CASE 
-                WHEN aid_type = 'I.A. Bilateral Official Development Assistance by types of aid (1+2+3+4+5+6+7+8+9+10)' THEN "value" 
-                ELSE 0
-                END
-            )  
-            -
-            SUM(CASE 
-                WHEN aid_type = 'I.A. Memo: ODA channelled through multilateral organisations' THEN "value" 
-                ELSE 0
-                END)AS "Bilateral funding",
-            SUM(CASE 
-                WHEN aid_type = 'I.A. Memo: ODA channelled through multilateral organisations' THEN "value" 
-                ELSE 0
-                END
-            ) AS "Bilateral as earmarked funding through multilaterals",
-            SUM(CASE 
-                WHEN aid_type = 'I.B. Multilateral Official Development Assistance (capital subscriptions are included with grants)' THEN "value" 
-                ELSE 0
-                END
-            ) AS "Multilateral as core contributions to organizations",
-            SUM(CASE 
-                WHEN aid_type = 'I. Official Development Assistance (ODA) (I.A + I.B)' THEN "value" 
-                ELSE 0
-                END
-            ) AS "Total ODA"
-        FROM base
-        GROUP BY 1,2
+    SELECT 
+        "Donor_1" AS donor,
+        "Year" AS year,
+        "Aid type" AS aid_type,
+        "VALUE" AS value
+    FROM "{{dac1_file}}"
+    WHERE 1=1
+    AND year BETWEEN ({{latest_year}} - 4) AND ({{latest_year}})
+    AND "Amount type" = 'Constant Prices (2022 USD millions)'
+    AND "Fund flows" = 'Grant equivalents'
+    AND "Aid type" IN (
+        'I. Official Development Assistance (ODA) (I.A + I.B)',
+        'I.B. Multilateral Official Development Assistance (capital subscriptions are included with grants)',
+        'I.A. Memo: ODA channelled through multilateral organisations',
+        'I.A. Bilateral Official Development Assistance by types of aid (1+2+3+4+5+6+7+8+9+10)'
     )
+), 
+
+filtered AS (  
     SELECT
         donor,
-        "Year",
-        "Total ODA",
-        coalesce("Bilateral funding", 0) AS "Bilateral funding",
-        coalesce("Multilateral as core contributions to organizations", 0) AS "Multilateral as core contributions to organizations", 
-        coalesce("Bilateral as earmarked funding through multilaterals", 0) AS "Bilateral as earmarked funding through multilaterals",
-        round( 100 * coalesce("Bilateral as earmarked funding through multilaterals", 0) / "Total ODA")::INT || '%' AS "Earmarked",
-        round( 100 * coalesce("Bilateral funding", 0) / "Total ODA")::INT || '%'  AS "Bilateral",
-        round( 100 * coalesce("Multilateral as core contributions to organizations", 0) / "Total ODA")::INT || '%' AS "Multilateral"
-    FROM filtered
-    ORDER BY "Year"
+        year AS "Year",
+        SUM(CASE 
+            WHEN aid_type = 'I.A. Bilateral Official Development Assistance by types of aid (1+2+3+4+5+6+7+8+9+10)' THEN "value" 
+            ELSE 0
+            END
+        )  
+        -
+        SUM(CASE 
+            WHEN aid_type = 'I.A. Memo: ODA channelled through multilateral organisations' THEN "value" 
+            ELSE 0
+            END)AS "Bilateral funding",
+        SUM(CASE 
+            WHEN aid_type = 'I.A. Memo: ODA channelled through multilateral organisations' THEN "value" 
+            ELSE 0
+            END
+        ) AS "Bilateral as earmarked funding through multilaterals",
+        SUM(CASE 
+            WHEN aid_type = 'I.B. Multilateral Official Development Assistance (capital subscriptions are included with grants)' THEN "value" 
+            ELSE 0
+            END
+        ) AS "Multilateral as core contributions to organizations",
+        SUM(CASE 
+            WHEN aid_type = 'I. Official Development Assistance (ODA) (I.A + I.B)' THEN "value" 
+            ELSE 0
+            END
+        ) AS "Total ODA"
+    FROM base
+    GROUP BY 1,2
+), 
+
+deflated AS (
+    SELECT 
+        f.donor,
+        f.year,
+        coalesce("Bilateral funding", 0) * dfl.deflator / 100 AS "Bilateral funding",
+        coalesce("Bilateral as earmarked funding through multilaterals", 0) * dfl.deflator / 100 AS "Bilateral as earmarked funding through multilaterals",
+        coalesce("Multilateral as core contributions to organizations", 0) * dfl.deflator / 100 AS "Multilateral as core contributions to organizations",
+        "Total ODA" * dfl.deflator / 100 AS "Total ODA",
+    FROM filtered f 
+    LEFT JOIN "{{deflator_file}}" dfl ON dfl.donor = f.donor AND dfl.year = {{latest_year}}
+)
+
+SELECT
+    donor,
+    "Year",
+    "Total ODA",
+    round("Bilateral funding", 2) AS "Bilateral funding",
+    round("Multilateral as core contributions to organizations", 2) AS "Multilateral as core contributions to organizations", 
+    round("Bilateral as earmarked funding through multilaterals", 2) AS "Bilateral as earmarked funding through multilaterals",
+    round( 100 * coalesce("Bilateral as earmarked funding through multilaterals", 0) / "Total ODA")::INT || '%' AS "Earmarked",
+    round( 100 * coalesce("Bilateral funding", 0) / "Total ODA")::INT || '%'  AS "Bilateral",
+    round( 100 * coalesce("Multilateral as core contributions to organizations", 0) / "Total ODA")::INT || '%' AS "Multilateral"
+FROM filtered
+ORDER BY "Year"
