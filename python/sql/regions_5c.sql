e34ee0527a7f3b46bf41aee139d5f7baa737c969
Robert West <rwest82396@gmail.com>
2025-03-10 19:39:52 +0100

    Update regions to use a different tab in the XLS file

--- python/sql/regions_5c.sql
+++ python/sql/regions_5c.sql
@@ -2,10 +2,11 @@ WITH base AS (
     SELECT
         donor_name,
         year,
-        region_name,
+        drf."DT_region" AS region_name,
         coalesce(sum(usd_disbursement_defl), 0) AS bilateral_oda,
         coalesce(sum(usd_disbursement_defl), 0) * 100 / sum(sum(usd_disbursement_defl)) OVER (PARTITION BY donor_name, year) AS share
-    FROM "{{crs_file}}"
+    FROM "{{crs_file}}" crs
+    LEFT JOIN "{{dt_regions_file}}" drf ON drf."microdata_region" = crs.region_name
     WHERE year BETWEEN ({{latest_year}} - 4) AND ({{latest_year}})
     AND donor_name IN {{dac_countries}}
     AND flow_name IN (
@@ -18,7 +19,7 @@ WITH base AS (
 SELECT 
     b.year AS year,
     b.region_name AS "Region",
-    b.bilateral_oda * (dfl.deflator / 100) AS "Bilateral ODA",
+    round(b.bilateral_oda * (dfl.deflator / 100), 2) AS "Bilateral ODA",
     round(b.share,1) || '%' AS "Share",
     b.donor_name AS donor,
 FROM base b
